f2af195285b4aea53aaac104f6a461fc3107d863
Ian McNaughton <IanMcNaugh@gmail.com>
2023-12-02 00:55:12 -0500

    got part 2, bah im coding late at night at my parents house, so forgive the slop

--- 2023/day2.js
+++ 2023/day2.js
@@ -2,23 +2,24 @@ import fs from 'fs'
 
 const input = fs.readFileSync('resources/day2.txt', 'utf-8')
 
-const expected = {
-  red: 12,
-  green: 13,
-  blue: 14
-}
 
 function parseGame(line){
+  const colorMax = {}
   let sep = line.indexOf(':');
-  const gameNumber = line.substring(5, sep)
   const pulls = line.substring(sep + 1, line.length).split(';').map(p => p.trim())
-  const allValidPulls = pulls.map(pull =>{
+  pulls.map(pull =>{
     return pull.split(',').map(colCount => colCount.trim()).map(cc => {
       const idk = cc.split(' ')
-      return parseInt(expected[idk[1]]) >= parseInt(idk[0])
-    }).reduce((previous, current) => (previous && current), true)
-  }).reduce((previous, current) => (previous && current), true)
-  return allValidPulls ? gameNumber : 0
+      if(!colorMax[idk[1]] || colorMax[idk[1]] < parseInt(idk[0])){
+        colorMax[idk[1]] = parseInt(idk[0])
+      }
+    })
+  })
+  let prod = 1
+  for(const c in colorMax){
+    prod = prod * parseInt(colorMax[c])
+  }
+  return prod
 }
 
 const output = input.split('\n').map(game =>{
