b2742a409b6fca527f5a92015cebb08e5bf3b6ef
Ian McNaughton <IanMcNaugh@gmail.com>
2023-12-04 11:14:40 -0600

    cleaner code, but read the rules ian, ugh so much lost time

--- 2023/day4.js
+++ 2023/day4.js
@@ -8,17 +8,33 @@ function getNumbersFromString(s, delim = ' ') {
   return s.split(delim).filter(s => s).map(a => parseInt(a))
 }
 
+let copy = []
+const lines = input.split('\n').length
+for(let g = 0; g < lines; g++){
+  copy.push(1)
+}
+
 const output = input.split('\n').map(line => {
+  const copies = copy[0]
+  copy = copy.splice(1, copy.length)
+
   const sMap = {}
   let w = 0
 
   const selectedAndWinners = line.split(':')[1].split('|')
+  const gameNumber = line.split(':')[0]
   const selected = getNumbersFromString(selectedAndWinners[0])
   const winners = getNumbersFromString(selectedAndWinners[1])
 
   selected.forEach(s => sMap[s] = true)
   winners.forEach(a => sMap[a] === true ? w++ : null)
-  return w ? Math.pow(2, w - 1) : 0
+
+  for(let i = 0; i < w; i++){
+    copy[i] = copy[i] + copies
+  }
+
+  console.log(`${gameNumber}, copies: ${copies}`)
+  return copies
 }).reduce((p, c) => c + p, 0)
 
 console.log(output)
