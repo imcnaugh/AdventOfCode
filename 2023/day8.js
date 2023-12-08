016fa05f82f8e768f3b211c972a92bd0e8366b3a
Ian McNaughton <IanMcNaugh@gmail.com>
2023-12-08 11:06:03 -0600

    im mad at myself, this would have worked, the brute force, but it was goign to take time. i cheated and saw an image posted on the subreddt, the paths are loops, so i need to refactor this and make it check for loops and do that math, bah

--- 2023/day8.js
+++ 2023/day8.js
@@ -8,20 +8,29 @@ const lines = input.split('\n')
 const instructions = lines[0].split('')
 const m = lines.splice(2)
 
+const startingNodes = []
+
 const map = {}
 m.forEach(line => {
   const parts = line.split('=').map(a => a.trim())
   const options = parts[1].slice(1, -1).split(', ').map(a => a.trim())
   map[parts[0]] = [options[0], options[1]]
+  if(parts[0][parts[0].length - 1] === 'A') startingNodes.push([parts[0]])
 })
 
-let currentNode = 'AAA'
-let stepCount = 0
+const strEndsInZ = (s) => s[2] === 'Z'
+const allNodesEndInZ = (n) => n.reduce((p, c) => p && strEndsInZ(c), true)
 
-while(currentNode !== 'ZZZ'){
+let nodes = startingNodes
+
+let stepCount = 0
+while(!allNodesEndInZ(nodes)){
   const dir = instructions[stepCount % instructions.length]
-  currentNode = map[currentNode][dir === 'L' ? 0 : 1]
+  nodes = nodes.map(node => {
+    return map[node][dir === 'L' ? 0 : 1]
+  })
   stepCount++
+  if(stepCount % 10000000 === 0) console.log(stepCount)
 }
 
-console.log(stepCount)
+console.log('finished: ', stepCount)
