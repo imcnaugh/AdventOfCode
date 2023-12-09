e2a273c7e171bc9931b1c8572211c98e73ee36ad
Ian McNaughton <IanMcNaugh@gmail.com>
2023-12-09 14:43:01 -0600

    added day 9 part 1

---
+++ 2023/day9.js
@@ -0,0 +1,28 @@
+import fs from 'fs'
+
+const isTest = false
+const input = fs.readFileSync(`resources/day9${isTest ? '.test' : ''}.txt`, 'utf-8')
+
+const isSequenceAllZeros = (seq) =>  seq.reduce((p, c) => p && c === 0, true)
+
+const generateNextSequence = (numArr) => {
+  let output = []
+  for(let i = 1; i < numArr.length; i++){
+    output.push(numArr[i] - numArr[i-1])
+  }
+  return output
+}
+
+const goDeeper = (seq) => {
+  if(isSequenceAllZeros(seq)) return 0
+  const nextSeq = generateNextSequence(seq)
+  const carry = goDeeper(nextSeq)
+  return carry + seq[seq.length - 1]
+}
+
+const output = input.split('\n').map(line => {
+  const initialNumbers = line.split(' ').map(Number)
+  return goDeeper(initialNumbers)
+}).reduce((p,c) => p+c)
+
+console.log(output)
