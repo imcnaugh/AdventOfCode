c596a44a0a0b3ee84b376a59f2e911334e6f05c3
Ian Mcnaughton <IanMcNaugh@gmail.com>
2024-12-01 00:05:17 -0600

    ok i think thats a good way of doing things, part 2 done

--- 2024/day1/src/main.rs
+++ 2024/day1/src/main.rs
@@ -1,7 +1,13 @@
+use std::collections::HashMap;
 use std::fs;
 
 fn main() {
-    let data = fs::read_to_string("resource/input.txt").unwrap();
+    let result = find_result("resource/input.txt");
+    println!("{result}");
+}
+
+fn find_result(path_string: &str) -> usize {
+    let data = fs::read_to_string(path_string).unwrap();
 
     let mut left_list: Vec<usize> = Vec::new();
     let mut right_list: Vec<usize> = Vec::new();
@@ -18,15 +24,52 @@ fn main() {
     left_list.sort();
     right_list.sort();
 
-    let mut diff: usize = 0;
+    let mut left_memo = HashMap::<usize, usize>::new();
+    let mut right_memo = HashMap::<usize, usize>::new();
 
-    for index in 0..left_list.len() {
-        if right_list[index] < left_list[index] {
-            diff += left_list[index] - right_list[index];
-        } else {
-            diff += right_list[index] - left_list[index];
+    let mut result: usize = 0;
+
+    left_list.iter().for_each(|i| {
+        match left_memo.get(i) {
+            Some(r) => result += r,
+            None => {
+                let count = get_count(*i, &right_list);
+                let count = count * i;
+                left_memo.insert(*i, count);
+                result += count;
+            }
         }
-    }
+    });
 
-    println!("{diff}");
+    // right_list.iter().for_each(|i| {
+    //     match right_memo.get(i) {
+    //         Some(r) => result += r,
+    //         None => {
+    //             let count = get_count(*i, &left_list);
+    //             right_memo.insert(*i, count);
+    //             result += count;
+    //         }
+    //     }
+    // });
+
+    result
+}
+
+fn get_count(i: usize, list: &Vec<usize>) -> usize {
+    list.iter().filter(|&m| -> bool {
+        i == *m
+    }).count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let result = find_result("resource/test.txt");
+        println!("{result}");
+
+        assert_eq!(31, result);
+    }
 }
