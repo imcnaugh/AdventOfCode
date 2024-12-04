dabcde5c6aac1657e62d054dbbee7425a3eeef18
Ian Mcnaughton <IanMcNaugh@gmail.com>
2024-12-04 11:10:06 -0600

    day 2

--- 2024/day4/src/main.rs
+++ 2024/day4/src/main.rs
@@ -1,18 +1,14 @@
 use std::fs;
 
 fn main() {
-    let result = part_1("resource/input.txt");
+    let result = part_2("resource/input.txt");
     println!("{result}");
 }
 
 fn part_1(path: &str) -> usize {
-    let text = fs::read_to_string(path).unwrap();
+    let grid = get_grid(path);
+
     let mut result = 0;
-
-    let grid = text.split("\n").map(|line| -> Vec<char> {
-        line.chars().into_iter().collect::<Vec<char>>()
-    }).collect::<Vec<Vec<char>>>();
-
     let directions = [(1i32, -1i32), (1,0), (1,1), (0, 1), (-1,1), (-1,0), (-1, -1), (0,-1)];
     let expected = ['X', 'M', 'A', 'S'];
 
@@ -40,6 +36,41 @@ fn part_1(path: &str) -> usize {
     result
 }
 
+fn part_2(path: &str) -> usize {
+    let grid = get_grid(path);
+    let mut result = 0;
+
+    let checks = [[(-1i32, -1i32), (1, 1)], [(-1, 1), (1, -1)]];
+
+    for row in 1..(grid.len() -1) {
+        'here: for col in 1..(grid[row].len() - 1) {
+            if grid[col][row] == 'A' {
+                for check in checks {
+                    let a = grid[(col as i32 + check[0].0) as usize][(row as i32 + check[0].1) as usize];
+                    let b = grid[(col as i32 + check[1].0) as usize][(row as i32 + check[1].1) as usize];
+                    if (a == 'M' && b == 'S') ||
+                        (a == 'S' && b == 'M'){
+                    } else {
+                        continue 'here;
+                    }
+                }
+
+                result += 1;
+            }
+        }
+    }
+
+    result
+}
+
+fn get_grid(path: &str) -> Vec<Vec<char>> {
+    let text = fs::read_to_string(path).unwrap();
+
+    text.split("\n").map(|line| -> Vec<char> {
+        line.chars().into_iter().collect::<Vec<char>>()
+    }).collect::<Vec<Vec<char>>>()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -48,5 +79,11 @@ mod tests {
     fn part_1_test() {
         let result = part_1("resource/test.txt");
         assert_eq!(18, result);
+    }
+
+    #[test]
+    fn part_2_test() {
+        let result = part_2("resource/test.txt");
+        assert_eq!(9, result);
     }
 }
