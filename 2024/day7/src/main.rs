580f44eb7a9986d07dbe78d63f2dae444698c53b
Ian Mcnaughton <IanMcNaugh@gmail.com>
2024-12-06 23:35:00 -0600

    fuck yeah day 7;

--- 2024/day7/src/main.rs
+++ 2024/day7/src/main.rs
@@ -2,7 +2,7 @@ use std::fs;
 
 fn main() {
     let input = get_string_from_file("resource/input.txt");
-    let result = part_1(input);
+    let result = part_2(input);
     println!("{result}");
 }
 
@@ -11,11 +11,72 @@ fn get_string_from_file(path: &str) -> String {
 }
 
 fn part_1(input: String) -> usize {
-    todo!()
+    input.lines().map(|line| -> usize {
+        let mut parts = line.split(":");
+        let total = parts.next().unwrap().parse::<usize>().unwrap();
+        let numbers: Vec<usize> = parts.next().unwrap().trim()
+            .split(" ")
+            .map(|n| n.parse::<usize>().unwrap())
+            .collect();
+
+        let operations = [
+            |a: usize, b:usize| -> usize {a + b},
+            |a: usize, b:usize| -> usize {a * b},
+        ];
+
+        if get_result(&numbers[..], 0, &operations[..], total){
+            return total
+        }
+        0
+    }).reduce(|a, b| a+b).unwrap()
+}
+
+fn get_result(nums: &[usize], current_total: usize, operations: &[fn(usize, usize)-> usize], expected: usize) -> bool {
+    if nums.is_empty() {
+        if current_total == expected {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    if current_total > expected {
+        return false;
+    }
+
+    let a = current_total;
+    let b = nums[0];
+    for op in operations {
+        let result = op(a, b);
+        if get_result(&nums[1..], result, &operations, expected) {
+            return true;
+        }
+    }
+
+    false
 }
 
 fn part_2(input: String) -> usize {
-    todo!()
+    input.lines().map(|line| -> usize {
+        let mut parts = line.split(":");
+        let total = parts.next().unwrap().parse::<usize>().unwrap();
+        let numbers: Vec<usize> = parts.next().unwrap().trim()
+            .split(" ")
+            .map(|n| n.parse::<usize>().unwrap())
+            .collect();
+
+        let operations = [
+            |a: usize, b:usize| -> usize {a + b},
+            |a: usize, b:usize| -> usize {a * b},
+            |a: usize, b:usize| -> usize {
+                format!("{a}{b}").parse::<usize>().unwrap()
+            },
+        ];
+
+        if get_result(&numbers[..], 0, &operations[..], total){
+            return total
+        }
+        0
+    }).reduce(|a, b| a+b).unwrap()
 }
 
 #[cfg(test)]
@@ -26,13 +87,14 @@ mod tests {
     fn test_part_1() {
         let input = get_string_from_file("resource/test.txt");
         let result = part_1(input);
-        assert_eq!(0, result);
+        assert_eq!(3749, result);
     }
 
+    #[test]
     fn test_part_2() {
         let input = get_string_from_file("resource/test.txt");
         let result = part_2(input);
-        assert_eq!(0, result);
+        assert_eq!(11387, result);
     }
 }
 
