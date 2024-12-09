d1dc016e9208b90aa654e88afe5d13f24a5f5a4a
Ian Mcnaughton <IanMcNaugh@gmail.com>
2024-12-09 16:36:19 -0600

    dam off by 1 errors are killing me

--- 2024/day9/src/main.rs
+++ 2024/day9/src/main.rs
@@ -2,7 +2,7 @@ use std::fs;
 
 fn main() {
     let input = get_file("resource/input.txt");
-    let result = part_1(input);
+    let result = part_2(input);
     println!("{result}");
 }
 
@@ -56,7 +56,84 @@ fn part_1(input:String) -> usize {
 }
 
 fn part_2(input:String) -> usize {
-    0
+    let chars = input.chars().map(|c| c.to_string().parse::<usize>().unwrap());
+
+    let mut list: Vec<Option<usize>> = vec![];
+
+    for (index, c) in chars.enumerate() {
+        if index % 2 == 1 {
+            for _ in 0..c {
+                list.push(None);
+            }
+        } else {
+            let file_index = index / 2;
+            for _ in 0..c {
+                list.push(Some(file_index));
+            }
+        }
+    }
+
+    let mut rev_index = list.len() - 1;
+    'all: while rev_index >= 0 {
+        let mut current_file_id = 0;
+        let mut current_file_size = 0;
+
+        if let Some(pid) = list[rev_index] {
+            current_file_id = pid;
+            current_file_size = 1;
+            loop {
+                if rev_index == 0 {
+                    break 'all;
+                }
+                rev_index -= 1;
+                if rev_index == 1 || list[rev_index].is_none() {
+                    break;
+                }
+                if let Some(cid) = list[rev_index] {
+                    if cid != pid {
+                        break;
+                    }
+                    current_file_size += 1;
+                }
+            }
+            // print_list(&list);
+
+            'here: for fi in 0..rev_index {
+                if list[fi].is_none() {
+                    for cj in 0..current_file_size {
+                        if list[fi + cj].is_some() {
+                            continue 'here;
+                        }
+                    }
+                    for n in 0..current_file_size {
+                        list.swap(fi + n, rev_index+n + 1);
+                    }
+                    break;
+                }
+            }
+        } else {
+            rev_index -= 1;
+        }
+    }
+
+    print_list(&mut list);
+
+    list.into_iter().enumerate().map(|(index, item)| -> usize {
+        match item {
+            Some(i) => index * i,
+            None => 0
+        }
+    }).sum()
+}
+
+fn print_list(list: &Vec<Option<usize>>) {
+    for x in list {
+        match x {
+            Some(n) => print!("{n}"),
+            None => print!(".")
+        }
+    }
+    println!()
 }
 
 #[cfg(test)]
@@ -74,6 +151,6 @@ mod tests {
     fn test_part_2() {
         let input = get_file("resource/test.txt");
         let result = part_2(input);
-        assert_eq!(0, result);
+        assert_eq!(2858, result);
     }
 }
