ca9f573d758d29e3bdc4580af4a5dd388f6446bb
Ian Mcnaughton <IanMcNaugh@gmail.com>
2024-12-15 16:39:32 -0600

    part 2 that was a cool part 2

--- 2024/day15/src/main.rs
+++ 2024/day15/src/main.rs
@@ -1,9 +1,12 @@
+use std::collections::HashSet;
 use std::fs;
 use crate::Direction::{Down, Left, Right, Up};
 use crate::Space::Wall;
 
+#[derive(Copy, Clone)]
 enum Space {
-    Box,
+    BoxLeft,
+    BoxRight,
     Wall,
 }
 
@@ -34,17 +37,17 @@ fn parse_input(input: String) -> (Vec<Vec<Option<Space>>>, Vec<Direction>, (usiz
     let mut robot_pos = (0usize, 0usize);
 
     let wh: Vec<Vec<Option<Space>>> = wh_str.lines().enumerate().map(|(row_index, line)| {
-        line.chars().enumerate().map(|(col_index, c) | -> Option<Space> {
+        line.chars().enumerate().map(|(col_index, c) | -> Vec<Option<Space>> {
             match c {
-                '#' => Some(Wall),
-                'O' => Some(Space::Box),
+                '#' => vec![Some(Wall), Some(Wall)],
+                'O' => vec![Some(Space::BoxLeft), Some(Space::BoxRight)],
                 '@' => {
-                    robot_pos = (col_index, row_index);
-                    None
+                    robot_pos = (col_index * 2, row_index);
+                    vec![None, None]
                 },
-                _ => None
+                _ => vec![None, None]
             }
-        }).collect()
+        }).flat_map(|i| i).collect()
     }).collect();
 
     let dir: Vec<Direction> = dir_str.chars().map(|c| match c {
@@ -74,7 +77,7 @@ fn calc_gps(wh: &Vec<Vec<Option<Space>>>) -> usize {
             match s {
                 Some(item) =>  {
                     match item {
-                        Space::Box => row_gps + col_index,
+                        Space::BoxLeft => row_gps + col_index,
                         _ => 0,
                     }
                 },
@@ -84,20 +87,80 @@ fn calc_gps(wh: &Vec<Vec<Option<Space>>>) -> usize {
     }).sum()
 }
 
-fn try_push(grid: &Vec<Vec<Option<Space>>>, cur_pos: (usize, usize), dir: &Direction) -> Option<(usize, usize)> {
-    loop {
-        let next_pos = get_next_pos(cur_pos, dir);
-        let next_space = &grid[next_pos.1][next_pos.0];
-        return match next_space {
-            Some(i) => {
-                match i {
-                    Space::Box => try_push(grid, next_pos, dir),
-                    Wall => None
+fn try_push(grid: &mut Vec<Vec<Option<Space>>>, cur_pos: (usize, usize), dir: &Direction) -> bool {
+    match dir {
+        Left => try_push_horizontal(grid, cur_pos, dir),
+        Right => try_push_horizontal(grid, cur_pos, dir),
+        Up => {
+            let mut positions = HashSet::new();
+            positions.insert(cur_pos);
+            try_push_vertically(grid, positions, dir)
+        },
+        Down => {
+            let mut positions = HashSet::new();
+            positions.insert(cur_pos);
+            try_push_vertically(grid, positions, dir)
+        },
+    }
+}
+
+fn try_push_vertically(grid: &mut Vec<Vec<Option<Space>>>, positions: HashSet<(usize, usize)>, dir: &Direction) -> bool {
+    let mut pos_to_check: HashSet<(usize, usize)> = HashSet::new();
+    for pos in &positions {
+        let next_pos = get_next_pos(*pos, &dir);
+        let item_at_next = grid[next_pos.1][next_pos.0];
+        if let Some(item) = item_at_next {
+            match item {
+                Space::BoxLeft => {
+                    pos_to_check.insert(next_pos);
+                    pos_to_check.insert((next_pos.0 + 1, next_pos.1));
                 }
-            },
-            None => Some(next_pos)
+                Space::BoxRight => {
+                    pos_to_check.insert(next_pos);
+                    pos_to_check.insert((next_pos.0 - 1, next_pos.1));
+                }
+                Wall => return false
+            }
         }
     }
+
+    let can_push = if pos_to_check.is_empty() {
+        true
+    } else {
+        try_push_vertically(grid, pos_to_check, dir)
+    };
+
+    if can_push {
+        for pos in &positions {
+            let next_pos = get_next_pos(*pos, &dir);
+            grid[next_pos.1][next_pos.0] = grid[pos.1][pos.0];
+            grid[pos.1][pos.0] = None;
+        }
+    }
+
+    can_push
+}
+
+fn try_push_horizontal(grid: &mut Vec<Vec<Option<Space>>>, cur_pos: (usize, usize), dir: &Direction) -> bool {
+    let next_pos = get_next_pos(cur_pos, dir);
+    let next_space = &grid[next_pos.1][next_pos.0];
+    let can_push =  match next_space {
+        Some(i) => {
+            match i {
+                Space::BoxLeft => try_push_horizontal(grid, next_pos, dir),
+                Space::BoxRight => try_push_horizontal(grid, next_pos, dir),
+                Wall => false
+            }
+        },
+        None => true
+    };
+
+    if can_push {
+        grid[next_pos.1][next_pos.0] = grid[cur_pos.1][cur_pos.0];
+        grid[cur_pos.1][cur_pos.0] = None
+    }
+
+    can_push
 }
 
 fn print_wh(grid: &Vec<Vec<Option<Space>>>, cur_pos: (usize, usize)) {
@@ -109,7 +172,8 @@ fn print_wh(grid: &Vec<Vec<Option<Space>>>, cur_pos: (usize, usize)) {
             match s {
                 Some(s) => match s {
                     Wall => '#',
-                    Space::Box => 'O'
+                    Space::BoxLeft => '[',
+                    Space::BoxRight => ']',
                 },
                 None => '.'
             }
@@ -123,29 +187,18 @@ fn part_1(input: String) -> usize {
 
     let mut cur_pos = start_pos;
 
+    print_wh(&grid, start_pos);
+
     for dir in directions {
-        match try_push(&grid, cur_pos, &dir) {
-            Some(next_free) => {
-                let next_pos = get_next_pos(cur_pos, &dir);
-                if next_free != next_pos {
-                    grid[next_pos.1][next_pos.0] = None;
-                    grid[next_free.1][next_free.0] = Some(Space::Box);
-                }
-                cur_pos = next_pos;
-            },
-            None => continue,
+        if try_push(&mut grid, cur_pos, &dir) {
+            cur_pos = get_next_pos(cur_pos, &dir);
         }
-        //
+
         // println!("{:?}", dir);
         // print_wh(&grid, cur_pos);
-
     }
 
     calc_gps(&grid)
-}
-
-fn part_2(input: String) -> usize {
-    todo!()
 }
 
 #[cfg(test)]
@@ -156,13 +209,6 @@ mod tests {
     fn test_part_1() {
         let input = get_file("resource/test.txt");
         let result = part_1(input);
-        assert_eq!(2028, result);
-    }
-
-    #[test]
-    fn test_part_2() {
-        let input = get_file("resource/test.txt");
-        let result = part_2(input);
-        assert_eq!(0, result);
+        assert_eq!(9021, result);
     }
 }
