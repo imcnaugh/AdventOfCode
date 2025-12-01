bf12b992cf271f97e6cbd1a6569638b76ad3e89b
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-11-30 22:06:11 -0600

    ohh i like that template

--- 2025/day_1/src/main.rs
+++ 2025/day_1/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../resource/input.txt");
+    let result = part_1(&input);
+    println!("Part 1: {}", result);
+}
+
+fn part_1(input: &str) -> usize {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        let input = include_str!("../resource/test.txt");
+        let result = part_1(&input);
+        assert_eq!(result, 12);
+    }
 }
