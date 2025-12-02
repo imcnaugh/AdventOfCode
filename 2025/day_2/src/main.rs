4c16c5832943a11abb06c9720834b50f908b1f69
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-12-02 11:09:07 -0600

    much more functional

--- 2025/day_2/src/main.rs
+++ 2025/day_2/src/main.rs
@@ -1,71 +1,52 @@
+use std::ops::RangeInclusive;
+
 fn main() {
     let input = include_str!("../resource/input.txt");
-    let result = part_2(input);
-    println!("Part 1: {}", result);
+    println!("Part 1 : {}", part_1(input));
+    println!("Part 2 : {}", part_2(input));
 }
 
 fn part_1(input: &str) -> usize {
-    input.split(',').fold(0, |acc, range| {
-        let s = range
-            .split('-')
-            .map(|n| n.parse::<usize>().unwrap())
-            .collect::<Vec<usize>>();
-        let start = s[0];
-        let end = s[1];
-
-        acc + (start..=end).fold(0, |acc, i| {
-            if check_for_kinda_sillyness(&i.to_string()) {
-                acc + i
-            } else {
-                acc
-            }
-        })
-    })
+    sum_silly_ids_in_ranges(input, check_for_sillynes)
 }
 
 fn part_2(input: &str) -> usize {
-    input.split(',').fold(0, |acc, range| {
-        let s = range
-            .split('-')
-            .map(|n| n.parse::<usize>().unwrap())
-            .collect::<Vec<usize>>();
-        let start = s[0];
-        let end = s[1];
-
-        acc + (start..=end).fold(0, |acc, i| {
-            if check_for_sillyness(&i.to_string()) {
-                acc + i
-            } else {
-                acc
-            }
-        })
-    })
+    sum_silly_ids_in_ranges(input, check_for_repeating_sillynes)
 }
 
-fn check_for_kinda_sillyness(input: &str) -> bool {
+fn sum_silly_ids_in_ranges(input: &str, is_silly: fn(&usize) -> bool) -> usize {
+    input
+        .split(',')
+        .map(|range| parse_range(range).filter(is_silly).sum::<usize>())
+        .sum()
+}
+fn parse_range(range: &str) -> RangeInclusive<usize> {
+    let s = range
+        .split('-')
+        .map(|n| n.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
+    s[0]..=s[1]
+}
+
+fn check_for_sillynes(input: &usize) -> bool {
+    let input = input.to_string();
     if input.len() % 2 != 0 {
         return false;
     }
 
     let mid = input.len() / 2;
-    input[0..mid] == input[mid..]
+    input[..mid] == input[mid..]
 }
 
-fn check_for_sillyness(input: &str) -> bool {
-    'outer: for i in 1..input.len() {
-        if input.len() % i != 0 {
-            continue;
-        }
-
-        let rep = &input[0..i];
-        for j in (0..input.len()).step_by(i) {
-            if input[j..j + i] != *rep {
-                continue 'outer;
-            }
-        }
-        return true;
-    }
-    false
+fn check_for_repeating_sillynes(input: &usize) -> bool {
+    let input = input.to_string();
+    (1..=input.len() / 2)
+        .filter(|i| input.len() % i == 0)
+        .any(|size| {
+            (0..input.len())
+                .step_by(size)
+                .all(|i| input[i..i + size] == input[0..size])
+        })
 }
 
 #[cfg(test)]
