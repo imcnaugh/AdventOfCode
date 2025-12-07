d01c5cf3c8dd8c31b1fb8c287da36c03db7bf71a
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-12-07 14:36:09 -0600

    cleaner

--- 2025/day_7/src/main.rs
+++ 2025/day_7/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashMap, HashSet};
+use std::str::Lines;
 
 fn main() {
     let input = include_str!("../resource/input.txt");
@@ -7,73 +8,57 @@ fn main() {
 }
 
 fn part_1(input: &str) -> usize {
-    let start = input.find('S').unwrap();
-    let lines = input.lines().collect::<Vec<&str>>();
-    let starter_set = HashSet::from([start]);
-    split(lines, 1, starter_set)
+    let mut lines = input.lines();
+    let first_line = lines.next().unwrap();
+    find_splits(&mut lines, HashSet::from([first_line.find('S').unwrap()]))
+}
+
+fn find_splits(lines_iter: &mut Lines, incoming_beams: HashSet<usize>) -> usize {
+    if let Some(line) = lines_iter.next() {
+        let chars = line.chars().collect::<Vec<char>>();
+        let mut next_beams = HashSet::new();
+        let mut splits = 0;
+
+        for i in incoming_beams {
+            if chars[i] == '^' {
+                splits += 1;
+                next_beams.insert(i - 1);
+                next_beams.insert(i + 1);
+            } else {
+                next_beams.insert(i);
+            }
+        }
+        splits + find_splits(lines_iter, next_beams)
+    } else {
+        0
+    }
 }
 
 fn part_2(input: &str) -> usize {
-    let start = input.find('S').unwrap();
-    let lines = input.lines().collect::<Vec<&str>>();
-    let mut starter_set: HashMap<usize, usize> = HashMap::new();
-    starter_set.insert(start, 1);
-    split_part_2(lines, 1, starter_set)
+    let mut lines = input.lines();
+    let first_line = lines.next().unwrap();
+    find_all_paths(
+        &mut lines,
+        HashMap::from([(first_line.find('S').unwrap(), 1)]),
+    )
 }
 
-fn split(input: Vec<&str>, current_line_index: usize, incoming: HashSet<usize>) -> usize {
-    if current_line_index == input.len() {
-        return 0;
-    }
-    let mut next_incoming = HashSet::new();
-    let current_line = input[current_line_index].chars().collect::<Vec<char>>();
-    let mut total_splits = 0;
-    for i in incoming {
-        if current_line[i] == '^' {
-            next_incoming.insert(i - 1);
-            next_incoming.insert(i + 1);
-            total_splits += 1;
-        } else {
-            next_incoming.insert(i);
-        }
-    }
-    split(input, current_line_index + 1, next_incoming) + total_splits
-}
-
-fn split_part_2(
-    input: Vec<&str>,
-    current_line_index: usize,
-    incoming: HashMap<usize, usize>,
-) -> usize {
-    if current_line_index == input.len() {
-        return incoming.values().sum::<usize>();
-    }
-    let mut next_incoming = HashMap::new();
-    let current_line = input[current_line_index].chars().collect::<Vec<char>>();
-    for (k, v) in incoming {
-        if current_line[k] == '^' {
-            let left = k - 1;
-            let right = k + 1;
-            if let Some(left_value) = next_incoming.get_mut(&left) {
-                *left_value += v;
+fn find_all_paths(lines_iter: &mut Lines, incoming_beams: HashMap<usize, usize>) -> usize {
+    if let Some(line) = lines_iter.next() {
+        let chars = line.chars().collect::<Vec<char>>();
+        let mut next_beams = HashMap::new();
+        for (k, v) in incoming_beams {
+            if chars[k] == '^' {
+                *next_beams.entry(k - 1).or_insert(0) += v;
+                *next_beams.entry(k + 1).or_insert(0) += v;
             } else {
-                next_incoming.insert(left, v);
-            }
-
-            if let Some(right_value) = next_incoming.get_mut(&right) {
-                *right_value += v;
-            } else {
-                next_incoming.insert(right, v);
-            }
-        } else {
-            if let Some(value) = next_incoming.get_mut(&k) {
-                *value += v;
-            } else {
-                next_incoming.insert(k, v);
+                *next_beams.entry(k).or_insert(0) += v;
             }
         }
+        find_all_paths(lines_iter, next_beams)
+    } else {
+        incoming_beams.values().sum::<usize>()
     }
-    split_part_2(input, current_line_index + 1, next_incoming)
 }
 
 #[cfg(test)]
