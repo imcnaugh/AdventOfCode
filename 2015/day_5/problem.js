a0614551692fe22a8cfd510643d0d4b0dc4718d0
Ian Mcnaughton <IanMcNaugh@gmail.com>
2026-01-10 23:05:22 -0600

    part 2

--- 2015/day_5/problem.js
+++ 2015/day_5/problem.js
@@ -10,7 +10,7 @@ const bad_combos = [
     ['x', 'y'],
 ];
 
-function is_nice(line) {
+function is_nice_part_1(line) {
     let previous = line[0]
 
     let vowel_count = vowels.includes(previous) ? 1 : 0;
@@ -35,10 +35,36 @@ function is_nice(line) {
     return is_a_repate && vowel_count >= 3;
 }
 
-// console.log(is_nice("xy"));
+// console.log(is_nice_part_1("xy"));
+
+function is_nice_part_2(line) {
+    let pair_seen_at = new Map();
+    let has_pair = false;
+    let odd_repeat = false;
+
+    for (let i = 0; i < line.length - 1; i++) {
+        let pair = line[i] + line[i + 1];
+
+        if (pair_seen_at.has(pair)) {
+            if (i >= pair_seen_at.get(pair) + 2) {
+                has_pair = true;
+            }
+        } else {
+            pair_seen_at.set(pair, i);
+        }
+
+        if (i < line.length - 2 && line[i] === line[i + 2]) {
+            odd_repeat = true;
+        }
+    }
+
+    return has_pair && odd_repeat;
+}
 
 let total = input.split('\n').filter((line) => {
-    return is_nice(line);
+    return is_nice_part_2(line);
 }).length;
 
 console.log(total);
+
+console.log(is_nice_part_2("xyxyx"));
