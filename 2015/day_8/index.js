ae652a64a00248ec3138170295f2362b55a886ef
Ian Mcnaughton <IanMcNaugh@gmail.com>
2026-01-13 22:26:42 -0600

    part 2

--- 2015/day_8/index.js
+++ 2015/day_8/index.js
@@ -3,15 +3,25 @@ import fs from 'fs';
 let input = fs.readFileSync('input.txt', 'utf8');
 // let input = fs.readFileSync('test.txt', 'utf8');
 
+// let output = input.split('\n').reduce((acc, line) => {
+//     let originalLength = line.length;
+//     line = line.slice(1, -1);
+//
+//     line = line.replace(/\\x[\da-f]{2}/g, "a");
+//     line = line.replace(/\\"/g, "a");
+//     line = line.replace(/\\\\/g, 'a');
+//
+//     return acc + originalLength - line.length;
+// }, 0)
+
 let output = input.split('\n').reduce((acc, line) => {
     let originalLength = line.length;
-    line = line.slice(1, -1);
+    line = line.replace(/\\/g, "\\\\");
+    line = line.replace(/"/g, "\\\"");
 
-    line = line.replace(/\\x[\da-f]{2}/g, "a");
-    line = line.replace(/\\"/g, "a");
-    line = line.replace(/\\\\/g, 'a');
+    // console.log(line);
 
-    return acc + originalLength - line.length;
-}, 0)
+    return acc + line.length - originalLength + 2;
+}, 0);
 
 console.log(output);
