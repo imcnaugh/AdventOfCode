c3027f166f48bc0a2443322f2c55bd8d399d09f8
Ian Mcnaughton <IanMcNaugh@gmail.com>
2026-01-24 18:18:38 -0600

    advent of reading comp again!

--- 2015/day_17/src/main.rs
+++ 2015/day_17/src/main.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 fn main() {
     let input = include_str!("../resources/input.txt");
     println!("Part 1: {}", part_1(input));
+    println!("Part 2: {}", part_2(input));
 }
 
 fn part_1(input: &str) -> usize {
@@ -13,6 +14,19 @@ fn part_1(input: &str) -> usize {
     containers.sort();
     let volume_to_fill = 150usize;
     ways_to_fill_container(volume_to_fill, &containers)
+}
+
+fn part_2(input: &str) -> usize {
+    let mut containers = input
+        .lines()
+        .map(|line| line.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
+    let volume_to_fill = 150usize;
+    let mut cache: Vec<Vec<usize>> = Vec::new();
+    ways_to_fill_container_tracking_containers(volume_to_fill, &containers, vec![], &mut cache);
+
+    let min = cache.iter().map(|c| c.len()).min().unwrap();
+    cache.iter().filter(|c| c.len() == min).count()
 }
 
 fn ways_to_fill_container(remaining_volume: usize, containers: &[usize]) -> usize {
@@ -29,6 +43,26 @@ fn ways_to_fill_container(remaining_volume: usize, containers: &[usize]) -> usiz
             let next_containers = &containers[index + 1..];
 
             acc + ways_to_fill_container(new_remaining_volume, &next_containers)
+        })
+}
+
+fn ways_to_fill_container_tracking_containers(remaining_volume: usize, containers: &[usize], current_in_use: Vec<usize>, cache: &mut Vec<Vec<usize>>) -> usize {
+    if remaining_volume == 0 {
+        cache.push(current_in_use.to_vec());
+        return 1;
+    }
+
+    containers
+        .iter()
+        .enumerate()
+        .filter(|(index, c)| *c <= &remaining_volume)
+        .fold(0usize, |acc, (index, con)| {
+            let new_remaining_volume = remaining_volume - *con;
+            let next_containers = &containers[index + 1..];
+            let mut used_containers = current_in_use.clone();
+            used_containers.push(*con);
+
+            acc + ways_to_fill_container_tracking_containers(new_remaining_volume, &next_containers, used_containers, cache)
         })
 }
 
