130a7ebf7ec7059e82d96c4daa5c0868f1771b36
Ian Mcnaughton <IanMcNaugh@gmail.com>
2026-02-07 22:12:07 -0600

    math, yeah
    And thats advent of code 2015, good stuff

---
+++ 2015/day_25/src/main.rs
@@ -0,0 +1,28 @@
+use regex::Regex;
+
+fn main() {
+    let input = include_str!("../resources/input.txt");
+    let rx = Regex::new(r"(\d+), (\d+)").unwrap();
+    let parts = rx.captures(input).unwrap();
+    let row = parts.get(1).unwrap().as_str().parse::<usize>().unwrap();
+    let col = parts.get(2).unwrap().as_str().parse::<usize>().unwrap();
+
+    let mult = 252533usize;
+    let divi = 33554393usize;
+
+    // let row = 3;
+    // let col = 3;
+
+    let start_of_row_num = (1..=row).enumerate().map(|(x, _)| x).sum::<usize>() + 1;
+    let index = (1..col).fold(start_of_row_num, |acc, x| acc + (row + x));
+
+    let code = (1..index).fold(20151125usize, |acc, index| {
+        let mul = acc * mult;
+        mul % divi
+    });
+
+    println!("row: {row}, col: {col}");
+    println!("start of row: {start_of_row_num}");
+    println!("index: {index}");
+    println!("code: {code}");
+}
