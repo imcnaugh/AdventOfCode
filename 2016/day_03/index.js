b9b760bd5b9b6282ec808b4afdbb83161daa60a1
Ian Mcnaughton <IanMcNaugh@gmail.com>
2026-07-30 16:01:03 -0500

    part 2

--- 2016/day_03/index.js
+++ 2016/day_03/index.js
@@ -1,15 +1,11 @@
 import fs from 'node:fs'
 
 const input = fs.readFileSync('input.txt', 'utf8')
-// const input = '  5   10  25'
-
 
 function isValidTriangle(numbers) {
     let sortedNums = numbers.sort((a, b) => a - b)
     return (sortedNums[0] + sortedNums[1]) > sortedNums[2]
 }
-
-
 
 const validTriangles = input.split('\n')
     .map(line => line.trim().split(/\s+/).map(Number))
@@ -19,3 +15,18 @@ const totalValidTriangles = validTriangles.length
 
 console.log(`Part 1: ${totalValidTriangles}`)
 
+const d2 = input.split('\n').map(line => line.trim().split(/\s+/).map(Number))
+const numbers = []
+for (let i = 0; i < 3; i++){
+    for(let j = 0; j < d2.length; j++){
+        numbers.push(d2[j][i])
+    }
+}
+
+let part2Valid = 0;
+for(let i = 0; i < numbers.length; i += 3){
+    if(isValidTriangle(numbers.slice(i, i + 3)))
+        part2Valid++
+}
+
+console.log(`Part 2: ${part2Valid}`)
