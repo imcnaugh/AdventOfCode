195e43a8041dc86d708cef83a88116883830d960
Ian Mcnaughton <IanMcNaugh@gmail.com>
2026-07-31 11:21:14 -0500

    part 1

--- 2016/day_07/index.js
+++ 2016/day_07/index.js
@@ -1,3 +1,25 @@
 import fs from 'node:fs'
 
 const input = fs.readFileSync('input.txt', 'utf8')
+// const input = "abcd[bddb]xyyx"
+
+const abbaRx = /(.)(?!\1)(.)\2\1/
+const bracketRx = /\[.*?]/g
+
+function isABBASupported(line) {
+    const bracketMatch = line.match(bracketRx);
+
+    for(const match of bracketMatch){
+        if(abbaRx.test(match)){
+            return false
+        }
+    }
+
+    return abbaRx.test(line)
+}
+
+const part1 = input.split('\n').filter(line => {
+    return isABBASupported(line)
+}).length
+
+console.log(`Part 1: ${part1}`)
