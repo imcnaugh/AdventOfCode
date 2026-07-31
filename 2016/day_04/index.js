15d985caf08d19bcd2efdc48af2e729d9bc21dcf
Ian Mcnaughton <IanMcNaugh@gmail.com>
2026-07-30 21:02:38 -0500

    day 4 part 2

--- 2016/day_04/index.js
+++ 2016/day_04/index.js
@@ -13,15 +13,28 @@ function generateChecksum(name){
         }
     })
 
-    const idk = Array.from(map).map(e => {
+    const sortedChars = Array.from(map).map(e => {
         return {char: e[0], count: e[1]}
     }).sort((a, b) => {
         if(a.count === b.count){
             return a.char.localeCompare(b.char)
         }
-        return b.count -g a.count
+        return b.count - a.count
     })
-    return idk.slice(0, 5).map(a => a.char).join('')
+    return sortedChars.slice(0, 5).map(a => a.char).join('')
+}
+
+function decryptName(name, sectorId) {
+    let rot = sectorId % 26;
+    return name.split('').map(c => {
+        if(c === '-')
+            return '-'
+        let a = c.charCodeAt(0) + rot
+        if(a > 'z'.charCodeAt(0)){
+            a -= 26
+        }
+        return String.fromCharCode(a)
+    }).join('')
 }
 
 const parsingRegex = /^([a-z-]*)(\d*)\[([a-z]*)]/
@@ -37,3 +50,20 @@ const sectorIdSum = input.split('\n').map(line => {
 }).map(row => row.sectorId).reduce((acc, c) => acc + c, 0)
 
 console.log(`Part 1: ${sectorIdSum}`)
+
+const expected = /northpole-object/ig
+// const expected = /north/i
+
+input.split('\n').map(line => {
+    const matches = line.match(parsingRegex)
+    return {
+        name: matches[1],
+        sectorId: parseInt(matches[2]),
+        checkSum: matches[3]
+    }
+}).forEach(line => {
+    let decrypted = decryptName(line.name, line.sectorId)
+    if(expected.test(decrypted)){
+        console.log(`Part 2: ${decrypted} ${line.sectorId}`)
+    }
+})
