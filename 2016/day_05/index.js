69dd642c3698379c2f91671873a9c37e2b19aff2
Ian Mcnaughton <IanMcNaugh@gmail.com>
2026-07-30 21:32:39 -0500

    part 2

--- 2016/day_05/index.js
+++ 2016/day_05/index.js
@@ -24,3 +24,29 @@ while(true) {
 
 const part1 = output.join('')
 console.log(`Part 1: ${part1}`)
+
+
+let matchPart2 = /^00000[0-7]/
+let outputPart2 = ['0','0','0','0','0','0','0','0']
+let placed = [false,false,false,false,false,false,false,false]
+let indexPart2 = 0;
+while(true) {
+    const toHash = input + indexPart2
+    const hashed = md5Hash(toHash)
+    if(matchPart2.test(hashed)) {
+        const pos = parseInt(hashed.charAt(5))
+        if(!placed[pos]){
+            outputPart2[pos] = hashed.charAt(6)
+            placed[pos] = true
+        }
+        const all = placed.reduce((acc, c) => acc && c, true)
+        if(all){
+            break
+        }
+    }
+    indexPart2++
+}
+
+
+const part2 = outputPart2.join('')
+console.log(`Part 2: ${part2}`)
