b6385457229c7cc0cba7c77fac436f8f81551b73
Ian Mcnaughton <IanMcNaugh@gmail.com>
2026-07-31 10:56:45 -0500

    part 1

--- 2016/day_06/index.js
+++ 2016/day_06/index.js
@@ -2,3 +2,26 @@ import fs from 'node:fs'
 
 const input = fs.readFileSync('input.txt', 'utf8')
 
+const indexedMaps = new Array(8).fill(null)
+for(let i = 0; i < 8; i++){
+    indexedMaps[i] = new Map()
+}
+
+input.split('\n').forEach(line => {
+    for(let i = 0; i < 8; i++){
+        const map = indexedMaps[i]
+        const char = line[i]
+
+        if(!map.has(char)){
+            map.set(char, 0)
+        }
+        map.set(char, map.get(char) + 1)
+    }
+})
+
+let part1 = indexedMaps.map(m => {
+    const sorted =  Array.from(m).sort((a, b) => b[1] - a[1])
+    return sorted[0][0]
+}).join('')
+
+console.log(`Part 1: ${part1}`)
